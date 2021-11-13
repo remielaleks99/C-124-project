eb5327fb1ac599020a7f497c71bdecec1ea64f6a
remielaleks99 <84626020+remielaleks99@users.noreply.github.com>
2021-11-13 12:33:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    canvas = createCanvas(550,550);
+    canvas.position(650,70);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+function modelLoaded(){
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
+function draw(){
+    background("#CCE6FF");
+}
